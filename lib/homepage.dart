07ea85d766c776cac0194513f5e599e0b0a7e3b8
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-01-09 16:11:31 +0530

    Playlist screen completed. Still need to optimise and fix few glitches.

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -34,25 +34,39 @@ class HomeScreen extends StatelessWidget {
             children: [
               const _DiscoverMusic(),
               _TrendingMusic(songs: songs),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    const SectionHeader(title: 'Playlist'),
-                    ListView.builder(
-                      padding: const EdgeInsets.only(top: 20.0),
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: playlists.length,
-                        itemBuilder: (context, index) {
-                          return PlaylistCard(playlist: playlists[index]);
-                        })
-                  ],
-                ),
-              ),
+              _PlaylistMusic(playlists: playlists),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _PlaylistMusic extends StatelessWidget {
+  const _PlaylistMusic({
+    Key? key,
+    required this.playlists,
+  }) : super(key: key);
+
+  final List<Playlist> playlists;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          const SectionHeader(title: 'Playlist'),
+          ListView.builder(
+            padding: const EdgeInsets.only(top: 20.0),
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: playlists.length,
+              itemBuilder: (context, index) {
+                return PlaylistCard(playlist: playlists[index]);
+              })
+        ],
       ),
     );
   }
--- lib/playlist_screen.dart
+++ lib/playlist_screen.dart
@@ -1,10 +1,215 @@
 import 'package:flutter/material.dart';
+import 'package:mplay/models/playlist_model.dart';
 
 class PlaylistScreen extends StatelessWidget {
   const PlaylistScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Playlist playlist = Playlist.playlists[0];
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.deepPurple.shade800.withOpacity(0.8),
+            Colors.deepPurple.shade200.withOpacity(0.8),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          centerTitle: true,
+          title: const Text('Playlist'),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                _PlaylistInformation(playlist: playlist),
+                const SizedBox(
+                  height: 30,
+                ),
+                const _PlayOrShuffle(),
+                _PlaylistSongs(playlist: playlist),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PlaylistSongs extends StatelessWidget {
+  const _PlaylistSongs({
+    Key? key,
+    required this.playlist,
+  }) : super(key: key);
+
+  final Playlist playlist;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: playlist.songs.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Text(
+            '${index + 1}',
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          title: Text(
+            playlist.songs[index].title,
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text(
+            '${playlist.songs[index].description} 03:15',
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+          ),
+          trailing:
+              const Icon(Icons.more_vert, color: Colors.white),
+        );
+      },
+    );
+  }
+}
+
+class _PlayOrShuffle extends StatefulWidget {
+  const _PlayOrShuffle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_PlayOrShuffle> createState() => _PlayOrShuffleState();
+}
+
+class _PlayOrShuffleState extends State<_PlayOrShuffle> {
+  bool isPlay = true;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isPlay = !isPlay;
+        });
+      },
+      child: Container(
+        height: 50.0,
+        width: width,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(15.0)),
+        child: Stack(
+          children: [
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 200),
+              left: isPlay ? 0 : width * 0.46,
+              child: Container(
+                height: 50.0,
+                width: width * 0.45,
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple.shade400,
+                    borderRadius: BorderRadius.circular(15.0)),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Play',
+                          style: TextStyle(
+                              color: isPlay ? Colors.white : Colors.deepPurple,
+                              fontSize: 17.0),
+                        ),
+                      ),
+                      const SizedBox(width: 10.0),
+                      Icon(
+                        Icons.play_circle,
+                        color: isPlay ? Colors.white : Colors.deepPurple,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Shuffle',
+                          style: TextStyle(
+                              color: isPlay ? Colors.deepPurple : Colors.white,
+                              fontSize: 17.0),
+                        ),
+                      ),
+                      const SizedBox(width: 10.0),
+                      Icon(
+                        Icons.shuffle,
+                        color: isPlay ? Colors.deepPurple : Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlaylistInformation extends StatelessWidget {
+  const _PlaylistInformation({
+    Key? key,
+    required this.playlist,
+  }) : super(key: key);
+
+  final Playlist playlist;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: Image.network(
+            playlist.imageUrl,
+            height: MediaQuery.of(context).size.height * 0.3,
+            width: MediaQuery.of(context).size.height * 0.3,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Text(
+          playlist.title,
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .copyWith(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
   }
 }
